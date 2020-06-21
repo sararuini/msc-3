7f2885234001cfe3bdc3c3f4ff298a542419cafe
sararuini <sara.ruini@hotmail.com>
2020-06-21 16:21:59 +0100

    initial setup

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+// router which routes with equivalent react components 
+
+import * as ROUTES from '../../constants/routes'; // access to routes file
+
+//Navigation compinent is rendered in 'App' Component
+const Navigation = () => (
+    <div>
+        <ul>
+            <li>
+                <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.LANDING}>Landing</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.HOME}>Home</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.ACCOUNT}>Account</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.ADMIN}>Admin</Link>
+            </li>
+        </ul>
+    </div>
+);
+
+export default Navigation;
